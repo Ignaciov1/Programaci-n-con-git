fc4f22bc835da03435c27b15e8041b0ed1787b08
ignacio <ig.sariego@duocuc.cl>
2025-06-22 19:09:29 -0400

    Primer commit. Programa de python que pide la cantidad de notas.

---
+++ notas.py
@@ -0,0 +1,14 @@
+def pedir_cantidad_notas():
+    while True:
+        try:
+            cantidad = int(input("Ingrese la cantidad de notas:"))
+            if cantidad > 1:
+                break
+            else:
+                print("Debes ingresar al menos 1 nota.")
+        except:
+            print("Valor invalido. Solo puedes ingresar numeros.")
+    return cantidad
+
+cantidad = pedir_cantidad_notas()
+print(cantidad)
